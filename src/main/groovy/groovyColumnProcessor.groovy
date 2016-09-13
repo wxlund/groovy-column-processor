d01b96e98a5781356e208ae0a390714e7dae9f85
wxlund <wxlund@yahoo.com>
2016-09-13 12:48:20 -0700

    Finished MVP

--- src/main/groovy/groovyColumnProcessor.groovy
+++ src/main/groovy/groovyColumnProcessor.groovy
@@ -12,56 +12,67 @@ static String toCamelCase( String text, boolean capitalized = false ) {
 }
 
 // def payload = "{member_id=4, region_code=NCAL, guid=null, uid=null, mrn=78472174, first_name=ALEXA, last_name=PARKER, middle_name=E, display_name=PARKER,ALEXA                  , gender=F, birth_date=1977-08-30 00:00:00.0, death_flag=null, death_date=null, email_address=prompttest102@yahoo.com, zip_code=11195     , invalid_mrn_code=null, mrn_replaced_by=null, last_rev_datetime=2014-09-13 00:00:00.0, last_verify_date=2014-09-13 10:45:35.35, update_datetime=2015-09-03 15:59:24.83, update_flag=null, suffix=null, blind_flag=null, deaf_flag=null, mute_flag=null, kpmcp_emp_flag=null, intrptr_req_code=null, mrn_prefix=11}"
-def payload = '{"member_id":"4" }' // , "region_code":"NCAL", "guid":"null", "uid":"null", "mrn":"78472174", "first_name":"ALEXA", "last_name":"PARKER", "middle_name":"E", "display_name":"PARKER,ALEXA", "gender":"F", "birth_date":"1977-08-30 00:00:00.0", "death_flag":"null", "death_date":"null", "email_address":"prompttest102@yahoo.com", "zip_code":"11195     ", "invalid_mrn_code":"null", "mrn_replaced_by":"null", "last_rev_datetime":"2014-09-13 00:00:00.0", "last_verify_date":"2014-09-13 10:45:35.35", "update_datetime":"2015-09-03 15:59:24.83", "update_flag":"null", "suffix":"null", "blind_flag":"null", "deaf_flag":"null", "mute_flag":"null", "kpmcp_emp_flag":"null", "intrptr_req_code":"null", "mrn_prefix":"11"}'
+def payload = '{"MEMBER_ID":"4", "region_code":"NCAL", "guid":"null", "uid":"null", "mrn":"78472174", "first_name":"ALEXA", "last_name":"PARKER", "middle_name":"E", "display_name":"PARKER,ALEXA", "gender":"F", "birth_date":"1977-08-30 00:00:00.0", "death_flag":"null", "death_date":"null", "email_address":"prompttest102@yahoo.com", "zip_code":"11195     ", "invalid_mrn_code":"null", "mrn_replaced_by":"null", "last_rev_datetime":"2014-09-13 00:00:00.0", "last_verify_date":"2014-09-13 10:45:35.35", "update_datetime":"2015-09-03 15:59:24.83", "update_flag":"null", "suffix":"null", "blind_flag":"null", "deaf_flag":"null", "mute_flag":"null", "kpmcp_emp_flag":"null", "intrptr_req_code":"null", "mrn_prefix":"11"}'
 
 // def s = new JsonBuilder(payload).toString()
 def columns = new JsonSlurper().parseText(payload)
+println columns
 
-// assert columns instanceof Map
-// println columns.getClass()
-
-assert columns instanceof Map
-assert columns.member_id == '4'
-println columns.getClass()
-
-List javaizedFieldNames = (List) new ArrayList();
+def javaBeanColumnNames = [:]
+List javaBeanColumnNameParts = []
 java.util.Hashtable dict = new java.util.Hashtable();
 
 for(Object m : columns ) {
     String column = m.toString();
     println "column: $column"
+    def columnName = ""
+    def javaBeanColumnName
+
     String[] keyAndValue = column.split("=")
     println keyAndValue
 
     if (keyAndValue[0] == keyAndValue[0].toLowerCase()) {
         def camelCaseColumn = toCamelCase(keyAndValue[0])
+        javaBeanColumnNames.put(camelCaseColumn, keyAndValue[1]);
         println camelCaseColumn
 
     } else {
+        // take care of special case where first word is in the dictionary
+        // assumed if we passed the toLowerCase() test above.
 
-    String edgeColumnNames = keyAndValue[0].split();
-
-    for (int i = 0; i < words.length; i++) {
-        if (dict.containsKey(words[i])) {
-            // special processing for Acronyms used by TPMG
-        } else {
-            if (i == 0) {
-                fieldName = words[0]
-                fieldName = fieldName.toLowerCase()
+        String[] ruleDrivenColumnNames = keyAndValue[0].split('_');
+        println "ruleDrivenColumnNames: $ruleDrivenColumnNames"
+        def columnNamePart = ""
+        for (int i = 0; i < ruleDrivenColumnNames.length; i++) {
+            if (dict.containsKey(ruleDrivenColumnNames[i])) {
+                // special processing for Acronyms used by TPMG
+                javaBeanColumnNameParts.add(ruleDrivenColumnNames[i]);
             } else {
-                fieldName = words[i]
-                fieldName = toCamelCase(fieldName, false)
-                println(fieldName)
-                javaizedFieldNames.add();
+                //  the first word may have been all caps but not in the dictionary. Just convert to lowercase.
+                if (i == 0) {
+                    columnNamePart = ruleDrivenColumnNames[0].toLowerCase()
+                    println "columnNamePart @ 0: $columnNamePart"
+                    javaBeanColumnNameParts.add(columnNamePart.toLowerCase());
+
+                } else {
+                    columnNamePart = ruleDrivenColumnNames[i]
+                    javaBeanColumnName = columnNamePart.toLowerCase().capitalize()
+                    println("$javaBeanColumnName: $javaBeanColumnName")
+                    javaBeanColumnNameParts.add(javaBeanColumnName);
+                }
+
 
             }
-
         }
+        println javaBeanColumnNameParts.join()
+        javaBeanColumnNames.put(javaBeanColumnNameParts.join(), keyAndValue[1]);
+
     }
 
-    return javaizedFieldNames;
-
 }
+
+println javaBeanColumnNames
+return javaBeanColumnNames;
 
 
 
